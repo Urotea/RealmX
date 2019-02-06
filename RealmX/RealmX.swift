8eaa25428b6edabc8108618881702ba94bffa044
Urotea <okakikakikaki@gmail.com>
2019-02-07 00:59:56 +0900

    Create doInTransaction method and test

--- RealmX/RealmX.swift
+++ RealmX/RealmX.swift
@@ -11,6 +11,10 @@ import RxSwift
 import RealmSwift
 
 public extension Results {
+    /// realm results to RxSwift Observable
+    /// This emits results when results updated.
+    ///
+    /// - Returns: RxSwift Observable
     public func toObservable() -> Observable<Results<Element>> {
         var token: NotificationToken?
         // RealmSwift oficial reference saies that self.realm will never return nil
@@ -39,6 +43,52 @@ public extension Results {
             return Disposables.create {
                 token?.invalidate()
             }
+        }
+    }
+}
+
+public extension Realm {
+    /// do in realm transaction with realm object.
+    ///
+    /// - Parameters:
+    ///   - object: this is handed over to "inTransaction".
+    ///   - inTransaction: this callback called in transaction.
+    /// - Returns: RxSwift Completable.
+    public func doInTransaction(
+        object: [Object],
+        inTransaction: @escaping ((Realm, [Object]) -> Void)) -> Completable {
+        return Completable.create { completable in
+            do {
+                try self.write {
+                    inTransaction(self, object)
+                }
+                completable(.completed)
+            } catch {
+                completable(.error(error))
+            }
+            return Disposables.create()
+        }
+    }
+
+    /// do in realm transaction with realm results.
+    ///
+    /// - Parameters:
+    ///   - results: this is handed over to "inTransaction".
+    ///   - inTransaction: this callback called in transaction.
+    /// - Returns: RxSwift Completable.
+    public func doInTransaction<Element: Object>(
+        results: Results<Element>,
+        inTransaction: @escaping ((Realm, Results<Element>) -> Void)) -> Completable {
+        return Completable.create { completable in
+            do {
+                try self.write {
+                    inTransaction(self, results)
+                }
+                completable(.completed)
+            } catch {
+                completable(.error(error))
+            }
+            return Disposables.create()
         }
     }
 }
